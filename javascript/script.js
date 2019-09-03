2698a7affa8c451ecdbc66d30dc6201bfe5ee813
Kanu <Kanu@kanusobti@gmail.com>
2019-09-03 02:09:36 -0400

    Removed extra unwanted code and checked code indentation.

--- javascript/script.js
+++ javascript/script.js
@@ -11,45 +11,39 @@
 const findEventApp = {};
 const genre = [`Sports`,`Music`,`Arts & Theatre`];//Array to select Random Event
 
-
+//Function to handle click events and call API
 findEventApp.getEvent = function() {
-
     $('#userSelectionForm').on('submit', function(event) {
-            event.preventDefault();
-            findEventApp.eventCity = $('input[name="city"]:checked').val();
-            console.log(findEventApp.eventCity);           
-            findEventApp.classificationName = $('#preferredGenre').find(":selected").text();
-            if(findEventApp.classificationName === `Random` || 
-            findEventApp.classificationName === `Select`)
-            {
-                let randomGenre = Math.floor(Math.random() * genre.length);
-                findEventApp.classificationName = genre[randomGenre];
-            }
-            console.log(findEventApp.classificationName);
-            findEventApp.getEventApi();
-            $('.header').hide();
-            $('.main').show();
+        event.preventDefault();
+        findEventApp.eventCity = $('input[name="city"]:checked').val();
+        findEventApp.classificationName = $('#preferredGenre').find(":selected").text();
+        //Find a random event if nothing is selected or if Random is selected
+        if(findEventApp.classificationName === `Random` || 
+        findEventApp.classificationName === `Select`)
+        {
+            let randomGenre = Math.floor(Math.random() * genre.length);
+            findEventApp.classificationName = genre[randomGenre];
+        }
+        findEventApp.getEventApi(); //Call Ticketmaster API.
+        $('.header').hide();
+        $('.main').show();
     });
 
+    //Handle Search Again button click. Reset all options
     $('#resetButtonDiv').on('click','a', function(event) {
-        console.log('I am Reset');
         $('.header').show();
         $('.main').hide();
         $('.wrapper').empty();
         $('input[name="city"]').prop('checked', false);
         $("#preferredGenre").val($("#preferredGenre option:first").val());
     });
-  
-
 }
 
-// storing user inputs int o variables, checking the inputs are valid, then passing to ajax request function
-
-
-///ajax call to the listener
+//Set up the Base URL & API Key
 findEventApp.baseUrl = `https://app.ticketmaster.com/discovery/v2/events.json`;
 findEventApp.apiKey = `fWG7RNp1homyItX8mZznhhwWgxC3upVy`;
 
+//Function declaration to call Ticketmaster API
 findEventApp.getEventApi = function(){
     $.ajax({
         url : findEventApp.baseUrl,
@@ -60,50 +54,38 @@ findEventApp.getEventApi = function(){
             city : findEventApp.eventCity,
             source : findEventApp.eventSource,
             classificationName : findEventApp.classificationName,
-            size : 4
-            
+            size : 4            
         }
     }).then(function(result){
         findEventApp.displayEvent(result);
     }).fail(
-        console.log('sorry')
+        console.log('Error in API Call')
     );
-
 }
+
+//Function to show API results on the UI.
 findEventApp.displayEvent = function(result){
-    // $('.reset').show();
     for(i = 0; i < result._embedded.events.length; i++){
-        // $('.wrapper').append(`<h4 class="name">${result._embedded.events[i].name}`);	
-        console.log(result);
         let imgId = `image${i}`;
-        console.log(imgId);
         let myImage = new Image();
         myImage.src = result._embedded.events[i].images[0].url;
         myImage.alt = `${result._embedded.events[i].name} image`
         myImage.className = `eventsImage`;
-        // $('.wrapper').append(myImage);	
-        console.log(result); 
         $('.wrapper').append(
         `<div id="eventsContainer${i}">
         <h4 class="name">${result._embedded.events[i].name}</h4>
-        <div id ="eventsDescription${i}"><p>${result._embedded.events[0]._embedded.venues[0].name}</br>${result._embedded.events[0]._embedded.venues[0].address.line1}</br>${result._embedded.events[0]._embedded.venues[0].city.name}</br>${result._embedded.events[0]._embedded.venues[0].postalCode}</p></div>
+        <div id ="eventsDescription${i}">
+        <p>${result._embedded.events[0]._embedded.venues[0].name}</br>${result._embedded.events[0]._embedded.venues[0].address.line1}</br>${result._embedded.events[0]._embedded.venues[0].city.name}</br>${result._embedded.events[0]._embedded.venues[0].postalCode}</p>
+        </div>
         </div>`);
-        console.log('my Div below');
-        console.log(document.getElementById(`eventsContainer${i}`));
         document.getElementById(`eventsDescription${i}`).appendChild(myImage);
         document.getElementById(`eventsContainer${i}`).className=`eventsContainer`;
         document.getElementById(`eventsDescription${i}`).className=`eventsDescription`;
-        // $('.wrapper').append(`<p class="dates">${result._embedded.events[i].name}`);	
-        // console.log(result);
     }
-    // $('.wrapper').append(
-    //     `<div class="eventsDescription" id="resetButtonDiv"><a class="resetButton button" id="resetButton">Search again!</a></div>`);
-
-    }
+}
 
 ///init function
 findEventApp.init = function(){
-    console.log('you are doing great');
     findEventApp.getEvent();
 }
 
